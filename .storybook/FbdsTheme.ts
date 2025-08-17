b1c3fc0b3ff59d8a1d1c89a058590cfab274bd77
podroks <bouilleteauflorian@outlook.fr>
2025-08-17 15:50:55 +0200

    💄 [main] Ajustement du theme pour les tooltip de storybook

--- .storybook/FbdsTheme.ts
+++ .storybook/FbdsTheme.ts
@@ -1,9 +1,21 @@
 import { create } from 'storybook/theming';
 
-export default create({
-  base: 'light',
-  brandTitle: 'Fbds - Storybook',
-  brandUrl: 'https://example.com',
-  brandImage: '/storybook-light.svg',
-  brandTarget: '_self',
-});
+export const darkTheme = create(
+  {
+    base: 'dark',
+    brandTitle: 'Fbds - Storybook',
+    brandUrl: 'https://example.com',
+    brandImage: '/storybook-dark.svg',
+    brandTarget: '_self',
+  }
+);
+
+export const lightTheme = create(
+  {
+    base: 'light',
+    brandTitle: 'Fbds - Storybook',
+    brandUrl: 'https://example.com',
+    brandImage: '/storybook-dark.svg',
+    brandTarget: '_self',
+  }
+);
